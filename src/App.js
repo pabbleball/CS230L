30ddeb3cdc2ae1cf4c65195f5ae4a0c34454c8d1
pabbleball <pss00011@mix.wvu.edu>
2024-02-23 17:35:19 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 01</h2>
+      <p>WVU ID: 800427583</p>
+      <p>Hi I am Preston</p>
+    </div>
+  );
+}
+
+export default App;
