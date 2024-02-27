5cb62e750f4581db7c018aa567b2d8a79d53857d
pabbleball <pss00011@mix.wvu.edu>
2024-02-27 15:18:54 -0500

    Week 8 Commit - v2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import 'Card.js';
+import 'Navbar.js';
 
 function App() {
   return (
     <div className="App">
+      Navbar();
       <h1>CS 230L</h1>
       <h2>Section - 01</h2>
       <p>WVU ID: 800427583</p>
       <p>Hi I am Preston</p>
+      Card();
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,27 +1,24 @@
 function Card() {
     return (
         <div class="card-deck">
-            <div class="card">
+            <div class="card text-center bg-primary">
                 <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title">Card 01</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
-            <div class="card">
+            <div class="card text-center bg-danger">
                 <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
-            <div class="card">
+            <div class="card text-center bg-success">
                 <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
             </div>
-    );
+    ); // Code from Bootstrap
 }
