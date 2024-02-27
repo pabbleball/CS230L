3ba1919ae1669565ff225c92393030d8050751dd
pabbleball <pss00011@mix.wvu.edu>
2024-02-27 15:24:55 -0500

    Week 8 Commit - v4

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import './Navbar.js';
 function App() {
   return (
     <div className="App">
-      Navbar();
+      <script>Navbar();</script>
       <h1>CS 230L</h1>
       <h2>Section - 01</h2>
       <p>WVU ID: 800427583</p>
       <p>Hi I am Preston</p>
-      Card();
+      <script>Card();</script>
     </div>
   );
 }
